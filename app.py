15f954f05b338bf51e78fd6e2db6162e16b511cc
math-n-code <100084915+math-n-code@users.noreply.github.com>
2022-03-08 22:09:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,7 @@ def prereg():
     if request.method == 'POST':
         email = request.form['email']
         # Check that email does not already exist (not a great query, but works)
+        return "Checked"
         if not db.session.query(User).filter(User.email == email).count():
             reg = User(email)
             db.session.add(reg)
